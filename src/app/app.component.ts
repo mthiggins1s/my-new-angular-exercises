fef726ba97629f1c394a40f8989c1a8eab6dcdc1
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-13 19:11:21 -0500

    exercise 1 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClickCounterComponent } from './click-counter/click-counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClickCounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/click-counter/click-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-click-counter',
+  imports: [],
+  templateUrl: './click-counter.component.html',
+  styleUrl: './click-counter.component.css'
+})
+export class ClickCounterComponent {
+  count = 0;
+
+  increaseCount() {
+    this.count ++;
+  }
+}
